66415aa185af0f50232ce7724b61ccdf2dce16f1
tbj <tbj@simcorp.com>
2017-02-07 09:26:05 +0100

    MovieStars

---
+++ MovieStars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MovieStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movieStars = new List<ObjMovieStar>();
+        }
+    }
+}
